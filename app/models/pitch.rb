7153e0a29ab9086fa22ceb2ede215a7accee75ea
Martino Visintin <vise890@gmail.com>
2012-11-17 16:14:34 +0000

    Add domain list to pitches

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,5 +1,5 @@
 class Pitch < ActiveRecord::Base
-  attr_accessible :description, :location, :title, :video_url, :skill_list, :email
+  attr_accessible :description, :location, :title, :video_url, :skill_list, :domain_list, :email
   acts_as_taggable
   acts_as_taggable_on :skills, :domains
 
