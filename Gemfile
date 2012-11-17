dc200124bf657343cebdc84f4ae2818b7460a29c
Martino Visintin <vise890@gmail.com>
2012-11-17 13:26:36 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'bootstrap-sass', '2.1'
+
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'haml-rails', "~> 0.3.5"
+
+  gem 'uglifier', '>= 1.0.3'
+  gem "lazy_high_charts", "~> 1.2.2"
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inception::Application.config.secret_token = '02de1801a689132f77429b4a1515cf3c84cb09f70c66467eaa5b81023d0ab3dc2ab25d3a2a6e38b732604cbfdef33f24334b527e7eda659ee4c5db1834b096f2'
