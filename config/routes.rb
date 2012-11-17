ce574d7af2540c2539ccd66d085f2d3b51435e63
Martino Visintin <vise890@gmail.com>
2012-11-17 20:37:28 +0000

    Create action for participation requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Inception::Application.routes.draw do
+  get "participation_requests/create"
+
   resources :pitches, except: [:edit, :update, :destroy] do
     resources :participation_requests, only: :create
   end
