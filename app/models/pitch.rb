43cfe9a79edc1cafb5fc8c59b895757849f3ddbc
Martino Visintin <vise890@gmail.com>
2012-11-17 16:09:37 +0000

    Validation on Pitches

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,4 +2,10 @@ class Pitch < ActiveRecord::Base
   attr_accessible :description, :location, :title, :video_url, :skill_list, :email
   acts_as_taggable
   acts_as_taggable_on :skills, :domains
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :video_url, presence: true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+
 end
