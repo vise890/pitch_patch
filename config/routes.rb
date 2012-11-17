3cdf85dc29398d151ddbcc7efb48a651dc7923a2
Martino Visintin <vise890@gmail.com>
2012-11-17 20:21:11 +0000

    Add participation_request route (only: create)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Inception::Application.routes.draw do
-  resources :pitches, except: [:edit, :update, :destroy]
+  resources :pitches, except: [:edit, :update, :destroy] do
+    resources :participation_requests, only: :create
+  end
 
   root to: 'pitches#index'
 end
