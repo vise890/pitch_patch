e37348ad3eabdfa9d8ee51a57d574d21c26a2d2d
Martino Visintin <vise890@gmail.com>
2012-11-17 14:16:41 +0000

    Add tokenise array method to Pitch

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,4 +2,13 @@ class Pitch < ActiveRecord::Base
   attr_accessible :description, :location, :title, :user_id, :video_url
   acts_as_taggable
   acts_as_taggable_on :skills, :domains
+
+  private
+    def tokenize_array(array)
+        result = ""
+        array.each do |e|
+          result += "<span class='label label-info'>" + e.to_s + "</span>&nbsp;"
+        end
+        return result
+    end
 end
