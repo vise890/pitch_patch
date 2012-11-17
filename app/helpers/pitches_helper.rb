8dfcebcece42bb03b5445fef03f689c161b2e4b1
Martino Visintin <vise890@gmail.com>
2012-11-17 23:10:35 +0000

    Add a super pimp youtube helper

--- app/helpers/pitches_helper.rb
+++ app/helpers/pitches_helper.rb
@@ -1,5 +1,13 @@
 module PitchesHelper
   def youtube_embed_url(video_url)
-    video_url.gsub('youtu.be', 'www.youtube.com/embed')
+    return '' if video_url.blank?
+
+    regexp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
+    youtube_code = video_url.match(regexp)
+    if youtube_code[7] && youtube_code[7].length == 11
+      "http://www.youtube.com/embed/#{youtube_code[7]}"
+    else
+      ''
+    end
   end
 end
