63743965aab644a2594f4bce947ee9cd115ae1f5
Martino Visintin <vise890@gmail.com>
2012-11-17 15:08:15 +0000

    Display tags on show pitches

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,13 +2,4 @@ class Pitch < ActiveRecord::Base
   attr_accessible :description, :location, :title, :video_url, :skill_list
   acts_as_taggable
   acts_as_taggable_on :skills, :domains
-
-  private
-    def tokenize_array(array)
-        result = ""
-        array.each do |e|
-          result += "<span class='label label-info'>" + e.to_s + "</span>&nbsp;"
-        end
-        return result
-    end
 end
