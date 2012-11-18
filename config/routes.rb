72d90a15cf17e7e01d7e50fe43fdd4eeb752abf6
Martino Visintin <vise890@gmail.com>
2012-11-18 11:45:37 +0000

    Users have pitches rel + CRUD on pitches

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Inception::Application.routes.draw do
 
   devise_for :users
 
-  resources :pitches, except: [:edit, :update, :destroy] do
+  resources :pitches do
     resources :participation_requests, only: :create
   end
 
