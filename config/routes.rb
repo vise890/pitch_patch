eb001c48a0cd789b811fdf54c8aa2203fa5902b5
Martino Visintin <vise890@gmail.com>
2012-11-18 10:36:26 +0000

    Add devise and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Inception::Application.routes.draw do
 
+  devise_for :users
+
   resources :pitches, except: [:edit, :update, :destroy] do
     resources :participation_requests, only: :create
   end
